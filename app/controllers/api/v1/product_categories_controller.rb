611b06ece9a05edc359d3b18e443136d76e75a73
Maxx <creepinjezus15@gmail.com>
2026-03-11 11:06:30 -0400

    Add Create Method

--- app/controllers/api/v1/product_categories_controller.rb
+++ app/controllers/api/v1/product_categories_controller.rb
@@ -3,6 +3,23 @@ class Api::V1::ProductCategoriesController < ApplicationController
     categories = ProductCategory
       .where(restaurant_id: current_restaurant.id)
       .order(:name)
+
     render json: categories
+  end
+
+  def create
+    normalized_name = category_params[:name].to_s.strip.titleize
+
+    category = current_restaurant.product_categories.find_or_create_by!(
+      name: normalized_name
+    )
+
+    render json: category, status: :created
+  end
+
+  private
+
+  def category_params
+    params.require(:product_category).permit(:name)
   end
 end
